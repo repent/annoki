69581446b2875c3358ac9ed55cfc55d3351baf75
Dan Hetherington <dan.hetherington@gmail.com>
2016-08-25 15:22:30 +0100

    Bind to 0.0.0.0 by default

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,14 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
+
+# Revert to pre 4.2 behaviour of binding development server to all ports
+
+require 'rails/commands/server'
+module Rails
+  class Server
+    def default_options
+      super.merge(Host:  '0.0.0.0', Port: 3000)
+    end
+  end
+end
